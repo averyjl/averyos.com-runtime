68cf6bc72eae89c66ce057348ec26c93fe40c1e5
Jason Lee Avery - Creator of T.A.I. (Truth Anchored Intelligence) AveryOS.com <truth@averyworld.com>
2026-02-10 02:13:08 -0700

    Add initial Next.js configuration file

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// next.config.js
+
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true,
+  basePath: '',
+  images: {
+    domains: ['averyos.com', 'averyjl.github.io'],
+  },
+};
+
+module.exports = nextConfig;
