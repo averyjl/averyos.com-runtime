c10ccc7e264931e707e025fc71b160a5814ce11c
Jason Lee Avery - Creator of T.A.I. (Truth Anchored Intelligence) AveryOS.com <truth@averyworld.com>
2026-02-24 04:43:39 -0700

    Reset Cloudflare config to default
    Removed custom configuration for Cloudflare adapter.

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,8 +1,4 @@
 // open-next.config.ts - Configuration for @opennextjs/cloudflare adapter
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
 
-// Using a Sovereign Bypass to force 'external' past the strict type wall
-export default defineCloudflareConfig({
-  minify: false, // Disabling to avoid further bundling friction
-  external: ["stripe"], 
-} as any);
+export default defineCloudflareConfig({});
